b58dae183a6d3fc8d35b485f41824051ca5584c5
Rudraksh Srivsatva <rudraksh2805@gmail.com>
2024-05-31 00:16:36 +0530

    Minor Changes to Input Component - Now exports both variants

--- frontend/src/components/input/input.jsx
+++ frontend/src/components/input/input.jsx
@@ -1,10 +1,18 @@
 import './input.css';
 
-export function Input({ fieldname='', placeholder='' }) {
+export function FieldsetInput({ fieldname='', placeholder='' }) {
     return (
         <div className='input-container'>
             {fieldname && <span className='fieldname'>{fieldname}</span>}
-            <input />
+            <input className='input' placeholder={placeholder}/>
+        </div>
+    )
+}
+
+export function Input ({ placeholder='' }) {
+    return (
+        <div className='bare-input-container'>
+            <input className='bare input' placeholder={placeholder}/>
         </div>
     )
 }
--- frontend/src/pages/echoes/page.jsx
+++ frontend/src/pages/echoes/page.jsx
@@ -1,13 +1,11 @@
-import { Input } from '../../components/input/input.jsx'
+import { FieldsetInput, Input } from '../../components/input/input.jsx'
 
 import './page.css';
 
 export function Homepage() {
     return (
         <div className='homepage-container'>
-            <Input />
-            <Input />
-            <Input />
+            <FieldsetInput />
             <Input />
             <Input />
             <Input />
