46627867af27e8fc6dc4b2f4811338123caf060c
Rudraksh Srivsatva <rudraksh2805@gmail.com>
2024-06-01 21:12:04 +0530

    Some work on the blog card component

--- frontend/src/pages/echoes/page.jsx
+++ frontend/src/pages/echoes/page.jsx
@@ -1,10 +1,12 @@
 import { Navbar } from '../../components/navbar/navbar'
+import { BlogFeature } from '../../components/blog-card/blog-card';
 import './page.css';
 
 export function Homepage() {
     return (
         <div className='homepage-container'>
             <Navbar />
+            <BlogFeature />
         </div>
     )
 }
