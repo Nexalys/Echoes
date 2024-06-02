5900ce25e4990c5f7595e962505840ad9988f244
wesha-904 <anwesha.singh2026@gmail.com>
2024-06-02 16:40:42 +0530

    created blog component

--- frontend/src/pages/echoes/page.jsx
+++ frontend/src/pages/echoes/page.jsx
@@ -1,10 +1,13 @@
 import { Navbar } from '../../components/navbar/navbar'
+import { Blog } from '../../components/blog/blog'
 import './page.css';
 
 export function Homepage() {
     return (
         <div className='homepage-container'>
             <Navbar />
+            <Blog />
         </div>
+
     )
 }
