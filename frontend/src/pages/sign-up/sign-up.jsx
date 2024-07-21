931a3a60d6324440ef13500e9f2cc62d565e3cfe
vibhor-5757 <vibhor.1bhatia@gmail.com>
2024-07-21 12:24:03 +0530

    updated signup page

--- frontend/src/pages/sign-up/sign-up.jsx
+++ frontend/src/pages/sign-up/sign-up.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+
+import circle1 from "../../../public/assets/circle1.svg"
+import circle2 from "../../../public/assets/circle2.svg"
+import circle3 from "../../../public/assets/circle3.svg"
+import circle4 from "../../../public/assets/circle4.svg"
+import circle5 from "../../../public/assets/circle5.svg"
+import circle6 from "../../../public/assets/circle6.svg"
 
 import "./sign-up.css";
 
@@ -23,67 +31,81 @@ export default function Signup() {
             </div>
 
             <form className="signup-form" onSubmit={handleSubmit(onSubmit)}>
-            <div className="signup-form-group">
-                    <label className="floating-label">First Name *</label>
-                <div className='signup-input-container'>
-                    <input className="signup-input" type="text" {...register("firstName", { required: true, maxLength: 80 })} />
+                <div className="signup-form-group">
+                        <label className="signup-floating-label">First Name *</label>
+                    <div className='signup-input-container'>
+                        <input className="signup-input" type="text" {...register("firstName", { required: true, maxLength: 80 })} />
+                    </div>
+                    {errors.firstName && <p className="signup-error-message">First name is required and should be less than 80 characters.</p>}
                 </div>
-                {errors.firstName && <p className="signup-error-message">First name is required and should be less than 80 characters.</p>}
-            </div>
 
-            <div className="signup-form-group">
-                <label className="floating-label">Last Name *</label>
-                <div className='signup-input-container'>
-                    <input className="signup-input" type="text" {...register("lastName", { required: true, maxLength: 100 })} />
+                <div className="signup-form-group">
+                    <label className="signup-floating-label">Last Name *</label>
+                    <div className='signup-input-container'>
+                        <input className="signup-input" type="text" {...register("lastName", { required: true, maxLength: 100 })} />
+                    </div>
+                    {errors.lastName && <p className="signup-error-message">Last name is required and should be less than 100 characters.</p>}
                 </div>
-                {errors.lastName && <p className="signup-error-message">Last name is required and should be less than 100 characters.</p>}
-            </div>
 
-            <div className="signup-form-group">
-                <label className="floating-label">Username *</label>
-                <div className='signup-input-container'>
-                    <input className="signup-input" type="text" {...register("username", { required: true })} />
+                <div className="signup-form-group">
+                    <label className="signup-floating-label">Username *</label>
+                    <div className='signup-input-container'>
+                        <input className="signup-input" type="text" {...register("username", { required: true })} />
+                    </div>
+                    {errors.username && <p className="signup-error-message">Username is required.</p>}
                 </div>
-                {errors.username && <p className="signup-error-message">Username is required.</p>}
-            </div>
 
-            <div className="signup-form-group">
-                <label className="floating-label">Email *</label>
-                <div className='signup-input-container'>
-                    <input className="signup-input" type="email" {...register("email", { 
-                    required: true, 
-                    pattern: /^[a-zA-Z][a-zA-Z0-9._%+-]*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/ 
-                    })} />
+                <div className="signup-form-group">
+                    <label className="signup-floating-label">Email *</label>
+                    <div className='signup-input-container'>
+                        <input className="signup-input" type="email" {...register("email", { 
+                        required: true, 
+                        pattern: /^[a-zA-Z][a-zA-Z0-9._%+-]*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/ 
+                        })} />
+                    </div>
+                    {errors.email && <p className="signup-error-message">Invalid email address. Ensure it starts with a letter and contains an "@" symbol.</p>}
                 </div>
-                {errors.email && <p className="signup-error-message">Invalid email address. Ensure it starts with a letter and contains an "@" symbol.</p>}
-            </div>
 
-            <div className="signup-form-group">
-                <label className="floating-label">Password *</label>
-                <div className='signup-input-container'>
-                    <input className="signup-input" type="password" {...register("password", { 
-                    required: true, 
-                    minLength: 8, 
-                    pattern: /^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[0-9]).{8,}$/,
-                    })} />
+                <div className="signup-form-group">
+                    <label className="signup-floating-label">Password *</label>
+                    <div className='signup-input-container'>
+                        <input className="signup-input" type="password" {...register("password", { 
+                        required: true, 
+                        minLength: 8, 
+                        pattern: /^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[0-9]).{8,}$/,
+                        })} />
+                    </div>
+                    {errors.password && <p className="signup-error-message">Password is required and must be at least 8 characters long, contain an uppercase letter, a special character, and a number.</p>}
                 </div>
-                {errors.password && <p className="signup-error-message">Password is required and must be at least 8 characters long, contain an uppercase letter, a special character, and a number.</p>}
-            </div>
 
-            <div className="signup-form-group">
-                <label className="floating-label">Confirm Password *</label>
-                <div className='signup-input-container'>
-                    <input className="signup-input" type="password" {...register("confirmPassword", { 
-                    required: true, 
-                    validate: value => value === password || "Passwords do not match"
-                    })} />
+                <div className="signup-form-group">
+                    <label className="signup-floating-label">Confirm Password *</label>
+                    <div className='signup-input-container'>
+                        <input className="signup-input" type="password" {...register("confirmPassword", { 
+                        required: true, 
+                        validate: value => value === password || "Passwords do not match"
+                        })} />
+                    </div>
+                    {errors.confirmPassword && <p className="signup-error-message">{errors.confirmPassword.message}</p>}
                 </div>
-                {errors.confirmPassword && <p className="signup-error-message">{errors.confirmPassword.message}</p>}
-            </div>
 
-            <input className="sign-up-button" type="submit" value="Sign-Up" />
+                <div className='signup-button-container'>
+                    <input className="signup-button" type="submit" value="Sign-Up" />
+                    <button className="signup-button" type="button">Sign-In with Google</button>
+                </div>
             </form>
+            <div className="signup-signin-link">
+                <Link to="/signin">Have an account? Sign in here.</Link>
+            </div>
+
         </div>
+
+        {/* <img src={circle1} alt="Circle 1" className="background-circle circle1" />
+        <img src={circle2} alt="Circle 2" className="background-circle circle2" />
+        <img src={circle3} alt="Circle 3" className="background-circle circle3" />
+        <img src={circle4} alt="Circle 4" className="background-circle circle4" />
+        <img src={circle5} alt="Circle 5" className="background-circle circle5" />
+        <img src={circle6} alt="Circle 6" className="background-circle circle6" /> */}
     </div>
   );
 }
