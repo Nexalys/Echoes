37de1d23fc88771de15db5a3cdea798633238368
vibhor-5757 <vibhor.1bhatia@gmail.com>
2024-07-22 12:44:20 +0530

    updated blog-small component and blog page

--- frontend/src/components/blog-small/blog-small.jsx
+++ frontend/src/components/blog-small/blog-small.jsx
@@ -1,86 +1,70 @@
+import React, { useRef, useEffect, useState } from 'react';
+import BlogImage from '../../../public/assets/blog-preview.jpeg';
 import './blog-small.css';
+import { AuthorDetails } from '../author-details/author-details';
 
+export function Blogsmall({summary = ("Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque mollitia soluta suscipit minima facilis sequi quod quia itaque repudiandae laboriosam deleniti labore, magnam perspiciatis maxime aperiam fugit neque odio voluptas autem ut. Consequatur tenetur velit corrupti dolorum quidem aliquam voluptatibus, aperiam porro vitae ratione, magnam quisquam voluptatum fuga? Eligendi ea incidunt et illum expedita praesentium accusamus. Maxime animi tempore saepe aspernatur totam repellat placeat, illo, accusantium ab, reprehenderit fuga fugit labore voluptatem autem repudiandae porro in! Odit suscipit omnis adipisci molestias officia maiores ab explicabo a eum obcaecati itaque dolor eius impedit rerum ad aliquam, excepturi debitis voluptatum repellendus expedita?"), subject = 'Subject' }) {
+    
+    const [showTopMask, setShowTopMask] = useState(false);
+    const [showBottomMask, setShowBottomMask] = useState(false);
+    const summaryRef = useRef(null);
 
-export function Blogsmall({username = "Username", date = "12th June 2024", url="https://i.pinimg.com/564x/74/5c/e7/745ce700e51c7112bce1213c2a61ddfa.jpg"}){
+    useEffect(() => {
+        const handleScroll = () => {
+            if (summaryRef.current) {
+                const { scrollTop, scrollHeight, clientHeight } = summaryRef.current;
+                const isScrollable = scrollHeight > clientHeight;
+                const isAtTop = scrollTop > 0;
+                const isAtBottom = scrollTop + clientHeight >= scrollHeight - 1; // A small margin to handle precision issues
 
-    return(
-    <div className="Blog-s">
-        <div className="left-content-s">
-            <h4>Blog Title</h4>
-            <hr />
-            <div className="blog-content-s">
-                <p>This is like a small portion of the blog. A trailer if you will. This is like a small portion of the blog. A trailer if you will.This is like a small portion of the blog. A trailer if you will. This is like a small portion of the blog. A trailer if you will.This is like a small portion of the blog. A trailer if you will. This is like a small portion of the blog. A trailer if you will.This is like a small portion of the blog. 
-                   A trailer if you will. This is like a small portion of the blog. A trailer if you will.This is like a small portion of the blog. A trailer if you will. This is like a small portion of the blog. A trailer if you will.</p>
+                setShowTopMask(isScrollable && isAtTop);
+                setShowBottomMask(isScrollable && !isAtBottom);
+            }
+        };
+
+        const element = summaryRef.current;
+        element.addEventListener('scroll', handleScroll);
+
+        // Initial check
+        handleScroll();
+
+        return () => {
+            element.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return (
+        <div className='blog-small-container'> 
+            <div className='blog-small-info'>
+                <h4>Blog Title</h4>
+                <span className='blog-small-line'></span>
+                <div
+                    className={`blog-summary ${showTopMask ? 'blog-mask-top' : ''} ${showBottomMask ? 'blog-mask-bottom' : ''}`}
+                    ref={summaryRef}
+                >
+                    {summary}
+                </div>
+                <div className='blog-small-auth-deets'><AuthorDetails /></div>
             </div>
-            <div className="footer-s">
-                <div className="profile-pic-s">
-                    <img src={url}/>
+            <div className='blog-small-pictures'>
+                <div className='blog-small-image-container'>
+                    <img src={BlogImage} />
                 </div>
-                <div className="author-s">
-                    <p>{username} ▪️</p>
-                </div>
-                <div className="publishing-date-s">{date}</div>
+                <div className='blog-small-icons'>
+                    <span className='blog-small-subject'>
+                        {subject}
+                    </span>
+                    <span className='blog-small-icons'>
+                        <div className='blog-small-circle-icon'>
+                            <i className="fa-solid fa-circle-minus"></i>
+                        </div>
 
-
-                <div className='pinned-s hidden' >
-                    <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <g id="tabler:pinned-filled">
-                                <path id="Vector" d="M14.6667 2.75C14.9003 2.75026 15.125 2.83972 15.2949 3.00011C15.4648 3.1605 15.567 3.37971 15.5807 3.61295C15.5944 3.84618 15.5185 4.07585 15.3686 4.25501C15.2186 4.43418 15.0059 4.54932 14.7739 4.57692L14.6667 4.58333V8.95033L16.4028 12.4236C16.4508 12.5186 16.4818 12.6213 16.4945 12.727L16.5 12.8333V14.6667C16.5 14.8912 16.4175 15.1079 16.2683 15.2757C16.1191 15.4435 15.9136 15.5506 15.6906 15.5769L15.5833 15.5833H11.9167V19.25C11.9164 19.4836 11.8269 19.7084 11.6666 19.8783C11.5062 20.0481 11.287 20.1504 11.0537 20.1641C10.8205 20.1778 10.5908 20.1019 10.4117 19.9519C10.2325 19.802 10.1173 19.5893 10.0898 19.3573L10.0833 19.25V15.5833H6.41667C6.19214 15.5833 5.97544 15.5009 5.80766 15.3517C5.63988 15.2025 5.53269 14.9969 5.50642 14.7739L5.5 14.6667V12.8333C5.50013 12.727 5.51874 12.6216 5.555 12.5217L5.59717 12.4236L7.33333 8.9485V4.58333C7.09969 4.58307 6.87497 4.49361 6.70508 4.33322C6.53519 4.17283 6.43295 3.95363 6.41926 3.72039C6.40557 3.48715 6.48145 3.25749 6.63141 3.07832C6.78137 2.89915 6.99408 2.78401 7.22608 2.75642L7.33333 2.75H14.6667Z" fill="#F64C46"/>
-                            </g>
-                        </svg><p>Pinned</p>
-                </div>
-
-                <div className='not-pinned-s '>
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M9 4V10L7 14V16H17V14L15 10V4M12 16V21M8 4H16" stroke="#F64C46" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-
-                </div>
-            </div> 
-        </div>  
-        <div className="right-content-s">
-            <div className="blog-icon-pic-s">
-                <div className="inner-circle-s">
-                    <img src={url}/>
-                </div>
-            </div>
-            <div className="bottom-box-s">
-                <div className="subject-s">Subject</div>
-
-                <div className="bookmark-s hidden">
-                    <div className="remove-s">
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <g id="gg:remove">
-                                <g id="Group">
-                                    <path id="Vector" d="M8 11C7.73478 11 7.48043 11.1054 7.29289 11.2929C7.10536 11.4804 7 11.7348 7 12C7 12.2652 7.10536 12.5196 7.29289 12.7071C7.48043 12.8946 7.73478 13 8 13H16C16.2652 13 16.5196 12.8946 16.7071 12.7071C16.8946 12.5196 17 12.2652 17 12C17 11.7348 16.8946 11.4804 16.7071 11.2929C16.5196 11.1054 16.2652 11 16 11H8Z" fill="#F64C46"/>
-                                    <path id="Vector_2" fillRule="evenodd" clipRule="evenodd" d="M23 12C23 18.075 18.075 23 12 23C5.925 23 1 18.075 1 12C1 5.925 5.925 1 12 1C18.075 1 23 5.925 23 12ZM21 12C21 13.1819 20.7672 14.3522 20.3149 15.4442C19.8626 16.5361 19.1997 17.5282 18.364 18.364C17.5282 19.1997 16.5361 19.8626 15.4442 20.3149C14.3522 20.7672 13.1819 21 12 21C10.8181 21 9.64778 20.7672 8.55585 20.3149C7.46392 19.8626 6.47177 19.1997 5.63604 18.364C4.80031 17.5282 4.13738 16.5361 3.68508 15.4442C3.23279 14.3522 3 13.1819 3 12C3 9.61305 3.94821 7.32387 5.63604 5.63604C7.32387 3.94821 9.61305 3 12 3C14.3869 3 16.6761 3.94821 18.364 5.63604C20.0518 7.32387 21 9.61305 21 12Z" fill="#F64C46"/>
-                                </g>
-                            </g>
-                        </svg>
-                    </div>
-                    <div className="saved-s">
-                        
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M5 21V5C5 4.45 5.196 3.97933 5.588 3.588C5.98 3.19667 6.45067 3.00067 7 3H17C17.55 3 18.021 3.196 18.413 3.588C18.805 3.98 19.0007 4.45067 19 5V21L12 18L5 21Z" fill="#F64C46"/>
-                        </svg>
-                    
-                    </div>
-                </div>
-                
-
-                <div className="not-saved-s ">
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g id="material-symbols:bookmark-outline">
-                            <path id="Vector" d="M3 23V3.44444C3 2.77222 3.238 2.19696 3.714 1.71867C4.19 1.24037 4.76152 1.00081 5.42857 1H17.5714C18.2393 1 18.8112 1.23956 19.2872 1.71867C19.7632 2.19778 20.0008 2.77304 20 3.44444V23L11.5 19.3333L3 23ZM5.42857 19.2722L11.5 16.6444L17.5714 19.2722V3.44444H5.42857V19.2722Z" fill="#F64C46"/>
-                        </g>
-                    </svg>
-
+                        <div className='blog-small-bookmark-icon'>
+                            <i className="fa-regular fa-bookmark"></i>
+                        </div>
+                    </span>
                 </div>
             </div>
-           
         </div>
-    </div>
-    )
+    );
 }
-
-export default Blogsmall;
--- frontend/src/pages/blogPage/blogPage.jsx
+++ frontend/src/pages/blogPage/blogPage.jsx
@@ -1,15 +1,43 @@
-import Markdown from 'react-markdown'
-// import balloon from '../../../public/assets/balloon.svg'
+import React, { useEffect, useState } from 'react';
+import Markdown from 'react-markdown';
 
-import { Navbar } from '../../components/navbar/navbar'
-import { ProfilePicture } from '../../components/profile-picture/profile-picture'
-import { Blog } from "../../components/blog/blog.jsx"
-import './blogPage.css'
+import { Navbar } from '../../components/navbar/navbar';
+import { ProfilePicture } from '../../components/profile-picture/profile-picture';
+import { Blog } from "../../components/blog/blog.jsx";
+import { Blogsmall } from "../../components/blog-small/blog-small.jsx";
 
-export function BlogPage({ width = "5.9%", blogTitle = "Blog Title", text = 'This is blog content using *markdown*', rightSidebarOptions = ["Main title", "Main title", "Main title", "Main title", "Main title", "Main title", "Main title", "Main title", "Main title"], username = "Username", date = "12th Jun 2024" }) {
-    const blogseekerstyle = {
-        width: width
-    }
+import './blogPage.css';
+
+export function BlogPage({ blogTitle = "Blog Title", text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias pariatur commodi assumenda blanditiis debitis, sapiente iusto dignissimos, dolores ipsa esse soluta optio repellat accusamus tempora accusantium ducimus dolore in aliquam ipsum provident. Totam optio impedit minima dolore possimus consequuntur expedita perferendis aspernatur fuga debitis qui sunt ullam odio assumenda, architecto quasi voluptatum reiciendis laudantium. Perspiciatis ullam dolores labore unde quae. Quo aperiam natus tempora numquam sunt perspiciatis suscipit doloribus expedita doloremque! Magni illum quam maxime, architecto esse minima corporis. Itaque, numquam optio. Doloremque impedit qui dolorum voluptas veniam esse illo, sapiente quasi velit debitis. Molestias provident totam, libero iure odit dolore cupiditate pariatur sunt repellat earum necessitatibus nisi assumenda consequuntur recusandae, id iusto itaque magnam quasi quis laboriosam nemo. Cumque maxime porro veniam dolor dignissimos eaque nostrum molestiae, nemo consectetur quidem? Alias, mollitia. Totam similique dicta ipsa dolorum mollitia perspiciatis maxime ut omnis sequi earum eveniet aliquam blanditiis ipsum atque, necessitatibus illo ad eligendi reiciendis fugiat eius tempore consectetur illum explicabo! Ratione cupiditate sunt alias error, sapiente aperiam quod corrupti. Iusto velit, ut ab iure animi nam deleniti eaque quibusdam doloribus aperiam. A culpa consectetur animi nam dignissimos voluptatem, odio tenetur, maxime vitae, nisi suscipit sit! Sint nemo, blanditiis quia alias totam unde expedita ea dicta, aliquid similique impedit laudantium necessitatibus culpa, eligendi qui sed nulla temporibus quisquam rem sit. Iste minima sunt, nobis optio quisquam error est, rem rerum quibusdam maxime architecto dolores eius eos labore, eaque inventore omnis accusamus cupiditate facilis. Aperiam voluptates, illo, incidunt minima pariatur blanditiis eligendi ad, possimus harum ullam necessitatibus nobis! Optio laboriosam nam tempora ad, deleniti quas eos corrupti dolore ipsum repudiandae dicta enim molestiae veritatis qui ducimus voluptas, sit quod voluptatem alias. Nisi nulla sit necessitatibus eos debitis! Nesciunt laboriosam, incidunt atque laudantium quod odio accusantium facilis voluptates ut. Totam corrupti laborum autem sit adipisci consequuntur, labore repudiandae excepturi saepe. Enim aperiam assumenda aliquam illum labore veniam explicabo consequuntur odit consequatur. In optio rem recusandae a quas nesciunt, architecto nemo veniam beatae iure sapiente dolores eos officiis harum quam maiores, totam assumenda necessitatibus id sit repudiandae unde amet excepturi! Voluptas delectus, minima, illum natus doloremque voluptate ducimus, possimus nostrum cum eius repellendus dolores hic rerum. Nisi neque voluptates sed sit ipsa dolorum, odit, facere cumque quas minima velit provident facilis illum! Ullam voluptates consectetur nostrum, deserunt provident velit fugiat ducimus aspernatur nemo commodi sapiente soluta earum assumenda, eveniet ea cum amet numquam. Esse cum, at doloremque cumque nulla quos totam repellat a est accusamus iure id illum, aut mollitia eaque nihil minima. Quibusdam non saepe modi, molestias iste quos doloremque debitis, sit aspernatur, odio ducimus velit? Deserunt qui nostrum necessitatibus minima ut eveniet dignissimos asperiores quisquam architecto corporis totam animi laudantium facilis odio ipsum ipsam, quia sit excepturi non adipisci ad distinctio! Laborum animi voluptate voluptates vel explicabo, vitae doloribus alias neque nulla quod laudantium quas aliquam sunt ut cumque autem quae deserunt corrupti incidunt adipisci iusto. Labore consequatur praesentium totam similique rerum dignissimos debitis numquam. Omnis pariatur dolorum, quas autem itaque consequuntur excepturi voluptas. Delectus rerum dolor odio, reprehenderit vitae exercitationem quibusdam sunt dicta officia eius incidunt aut asperiores veritatis iste doloribus sequi natus quia maiores veniam quos? Expedita officia consequatur fuga eius nihil inventore quo, tempore sapiente debitis similique porro corrupti, dolorum nostrum odio reprehenderit neque dolore molestiae praesentium, dolores reiciendis architecto. Praesentium accusantium eveniet soluta? Tempore optio ducimus quibusdam omnis non explicabo, harum corrupti provident, quasi soluta blanditiis quaerat temporibus! Ab provident eaque animi quam, aut natus quo hic magnam, quae molestias voluptate facilis in nihil, facere distinctio soluta expedita? Veniam deleniti vitae corporis corrupti molestias facere rerum enim porro cupiditate officiis expedita commodi sapiente dicta dolore inventore id fuga, ab, repudiandae alias, sint quibusdam. Ratione quaerat tempore nemo dolorem repudiandae rerum officiis sit. Recusandae ab tempore fuga dolore ullam culpa corrupti distinctio. Ipsa quia quaerat ratione delectus minima autem est, harum beatae ipsum unde, illum sapiente, exercitationem alias nostrum ullam sequi assumenda voluptatum velit repudiandae dignissimos quos. Quis, ad? Tenetur, soluta recusandae at velit fuga molestiae assumenda explicabo hic labore, excepturi veritatis maiores eveniet nesciunt voluptas maxime eligendi quasi tempora optio commodi. Hic, dolore similique obcaecati, optio praesentium dolores nam quod blanditiis dolorem exercitationem quas sapiente reiciendis nulla temporibus, quam deleniti? Necessitatibus inventore obcaecati libero ex tempore voluptates. Libero incidunt veritatis ipsa, officiis quidem nesciunt mollitia veniam ea numquam aliquid eaque eligendi magnam quia unde, in voluptatibus quae laudantium ad, qui odit molestiae fuga nihil laborum repellat. Perspiciatis, aspernatur! Amet, maiores excepturi? Iure harum quisquam cumque quis eius molestiae quasi nihil cum, molestias nostrum expedita est corporis cupiditate quaerat alias nemo eum corrupti nesciunt asperiores magnam? Necessitatibus qui quidem illo aliquid hic explicabo deserunt facere consequuntur nam quam ipsum quaerat cumque voluptates est at, alias mollitia, exercitationem quisquam, repellendus doloremque eligendi suscipit magni nostrum. Unde distinctio aliquam obcaecati, soluta itaque ullam numquam voluptas in ut nemo ab sequi dolorem voluptatum, laudantium pariatur, beatae dolores sed laboriosam quos sunt et ducimus esse deleniti? Earum explicabo a ratione consequuntur, autem sint incidunt officia ipsa tenetur doloribus mollitia officiis totam quam tempora esse distinctio reiciendis praesentium inventore quaerat facilis. Quidem earum numquam tempora veniam, nesciunt aspernatur eveniet provident, deleniti magnam iusto voluptatem perspiciatis tempore inventore omnis est sunt dolore nisi. Repudiandae fugiat dolorum, voluptas possimus aliquid vel tenetur laborum expedita veniam quas cupiditate quaerat, nisi cumque dicta autem reprehenderit vero quae voluptates officiis quos velit consequatur aperiam dolor! Officiis itaque ea assumenda exercitationem soluta voluptates veniam! Molestiae reiciendis necessitatibus optio natus! Debitis nisi ratione praesentium atque nobis at, molestiae dignissimos eum, explicabo earum cupiditate maxime fuga reiciendis enim voluptatem dicta repellendus aperiam ipsam architecto esse fugiat omnis! Nisi perferendis facilis in cumque ratione eveniet quidem magni, quos voluptas, omnis culpa repellat ducimus similique consectetur aliquam repudiandae sequi impedit sunt molestias quasi aperiam. Quaerat cum ab impedit ipsam cumque! Cumque quidem maxime laborum placeat, maiores, magni dignissimos perspiciatis expedita voluptates sapiente ducimus aut distinctio unde fugit error accusantium saepe adipisci, optio asperiores vero suscipit architecto? Excepturi veniam molestiae totam dolorem eligendi fugit!", rightSidebarOptions = ["Main title", "Main title", "Main title", "Main title", "Main title", "Main title", "Main title", "Main title", "Main title"], username = "Username", date = "12th Jun 2024" }) {
+    const [blogSeekerWidth, setBlogSeekerWidth] = useState("0%");
+
+    useEffect(() => {
+        const blogContent = document.querySelector('.blog-content');
+
+        const checkScroll = () => {
+            const scrollHeight = blogContent.scrollHeight;
+            const clientHeight = blogContent.clientHeight;
+            if (scrollHeight > clientHeight) {
+                setBlogSeekerWidth("0%");
+            } else {
+                setBlogSeekerWidth("100%");
+            }
+        };
+
+        const handleScroll = () => {
+            const scrollTop = blogContent.scrollTop;
+            const scrollHeight = blogContent.scrollHeight - blogContent.clientHeight;
+            const scrollPercentage = (scrollTop / scrollHeight) * 100;
+            setBlogSeekerWidth(`${scrollPercentage}%`);
+        };
+
+        checkScroll();
+        blogContent.addEventListener('scroll', handleScroll);
+
+        return () => {
+            blogContent.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     const handleClick = (index) => {
         const elements = document.querySelectorAll('.blog-right-sidebar .element');
@@ -28,7 +56,7 @@ export function BlogPage({ width = "5.9%", blogTitle = "Blog Title", text = 'Thi
             <Navbar />
             <div className='blog-page-container'>
                 <div className='blog-seeker'>
-                    <div className='blog-seeker-marker' style={blogseekerstyle}></div>
+                    <div className='blog-seeker-marker' style={{ width: blogSeekerWidth }}></div>
                 </div>
 
                 <div className='blog-content-container'>
@@ -98,11 +126,11 @@ export function BlogPage({ width = "5.9%", blogTitle = "Blog Title", text = 'Thi
                         <h3>More From </h3> <h2>&nbsp;Username</h2>
                     </div>
                     <div className='blogpage-blog-username'>
-                        <Blog />
-                        <Blog />
-                        <Blog />
-                        <Blog />
-                        <Blog />
+                        <Blogsmall />
+                        <Blogsmall />
+                        <Blogsmall />
+                        <Blogsmall />
+                        <Blogsmall />
                     </div>
                 </div>
 
@@ -112,14 +140,14 @@ export function BlogPage({ width = "5.9%", blogTitle = "Blog Title", text = 'Thi
                         <h3>More On </h3> <h2>&nbsp;This Topic</h2>
                     </div>
                     <div className='blogpage-blog-username'>
-                        <Blog />
-                        <Blog />
-                        <Blog />
-                        <Blog />
-                        <Blog />
+                        <Blogsmall />
+                        <Blogsmall />
+                        <Blogsmall />
+                        <Blogsmall />
+                        <Blogsmall />
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
